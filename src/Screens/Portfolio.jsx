dc758701bdf68264904592ab21b9247790083119
Zeeshan Siddiqui <zeeshansd767@gmail.com>
2024-10-27 00:35:43 -0700

    Your commit changes

--- src/Screens/Portfolio.jsx
+++ src/Screens/Portfolio.jsx
@@ -5,7 +5,7 @@ import { Card2 } from '../Components/Card'
 const Portfolio = () => {
   return (
     <div>
-      <Card2/>
+      <Card2 image='https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg' title='Ecommerce Website' description='Ecommerce Website using React JS and Tailwind CSS' link='https://shoopperr.netlify.app/'/>
     </div>
   )
 }
